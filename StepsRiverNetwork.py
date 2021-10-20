f8c4e9c252640343a80d270ac8fcd9022b01d278
sascha <sascha.bub@gmx.de>
2021-10-20 12:35:11 +0200

    updated version number and documentation

--- StepsRiverNetwork.py
+++ StepsRiverNetwork.py
@@ -13,6 +13,7 @@ class StepsRiverNetwork(base.Component):
     """The component encapsulating the Steps environmental fate module."""
     # RELEASES
     VERSION = base.VersionCollection(
+        base.VersionInfo("2.1.0", "2021-10-20"),
         base.VersionInfo("2.0.7", "2021-10-19"),
         base.VersionInfo("2.0.6", "2021-10-12"),
         base.VersionInfo("2.0.5", "2021-10-11"),
@@ -104,6 +105,7 @@ class StepsRiverNetwork(base.Component):
     VERSION.changed("2.0.5", "Replaced legacy format strings by f-strings")
     VERSION.changed("2.0.6", "Switched to Google docstring style")
     VERSION.changed("2.0.7", "Specified working directory for module")
+    VERSION.changed("2.1.0", "Replaced shapefile input")
 
     def __init__(self, name, observer, store):
         """
